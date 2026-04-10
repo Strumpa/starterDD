076f9f73ea8dca8bffc06a16ef3e73f33eddac44
Strumpa <raphael.guasch@polymtl.ca>
2026-04-09 22:50:23 -0400

    fixed typo in tutorials comments

--- tutorials/AT10_4x4_mix_splitting.py
+++ tutorials/AT10_4x4_mix_splitting.py
@@ -1,5 +1,5 @@
 # Example of a simple DRAGON 2-levels flux calculation scheme with starterDD
-# Generate a glow geometry with a single AT10 3x3 sub-assembly with 9 pins,
+# Generate a glow geometry with a single AT10 4x4 sub-assembly with 16 pins,
 # Run dragon with a RSE+IC self shielding step + first level IC on 295g + second level 26g MOC flux calculation.
 # At self shielding step and 1st level flux calculation, each pin is divided in Santamarina radial zones, 
 # Material Mixes are numbered by material ie enrichment / fuel type. 
