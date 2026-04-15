95449d4c5985ad0e99b7b84415346b1e4eb66038
Strumpa <raphael.guasch@gmail.com>
2026-04-15 13:21:36 -0400

    set up 010_inclued_porosity_calcultion branch

--- starterDD/GeometryAnalysis/__init__.py
+++ starterDD/GeometryAnalysis/__init__.py
@@ -1,5 +1,6 @@
 """GeometryAnalysis module - TDT file parsing and geometry analysis."""
 
 from .tdt_parser import read_material_mixture_indices_from_tdt_file
+import geometry_analysis
 
-__all__ = ["read_material_mixture_indices_from_tdt_file"]
+__all__ = ["read_material_mixture_indices_from_tdt_file", "geometry_analysis"]
---
+++ starterDD/GeometryAnalysis/geometry_analysis.py
@@ -0,0 +1,2 @@
+# geometry_analysis.py : 
+# analyse full core geometry and compute 3D porosity and hydraulic diameter maps.
